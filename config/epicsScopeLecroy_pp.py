63f30cf69550618fa6c02fd73fb3ccb02cce2c0a
Sukhanov, Andrei <sukhanov@bnl.gov>
2026-02-18 22:25:06 -0500

    removed config/epicsScopeLecroy_pp.py

--- config/epicsScopeLecroy_pp.py
+++
@@ -1,7 +0,0 @@
-"""Pypet definition for RIGOL epicsScope"""
-import epicsScope_pp as module
-
-def PyPage(**_):
-    return  module.PyPage(instance='Rigol:', title='RIGOL DHO924',
-        channels=4)
-
